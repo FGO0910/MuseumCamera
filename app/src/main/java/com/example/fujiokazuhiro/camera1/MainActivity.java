7cc07c6e0f6f309c5835b5a0e348866c9b71177a
藤尾和裕 <fujiokazuhiro@fujiokazuhiro-no-MacBook-Air.local>
2016-11-10 12:52:48 +0900

    四角形検出＆画像埋め込み成功

--- app/src/main/java/com/example/fujiokazuhiro/camera1/MainActivity.java
+++ app/src/main/java/com/example/fujiokazuhiro/camera1/MainActivity.java
@@ -122,104 +122,120 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
 
     // ここに画像処理
+//    @Override
+//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+//        Mat image = inputFrame.rgba();
+//        Mat gray = new Mat(3, 3, CvType.CV_32FC2);
+//        Mat dst_bin = new Mat(3, 3, CvType.CV_32FC2);
+//        Mat hierarchy = new Mat(3, 3, CvType.CV_32FC2);
+//        Resources r = getResources();
+//        Bitmap bmp_frame = BitmapFactory.decodeResource(r, R.drawable.image);
+////        bmp_frame = Bitmap.createScaledBitmap(bmp_frame, 640, 480, false);
+//        Mat mat = new Mat(3,3,CvType.CV_32FC2);
+//        Core.add(mat, image, image);
+//        Imgproc.pyrDown(image, image);
+//        Imgproc.pyrUp(image, image);
+//        Imgproc.cvtColor(image , gray, Imgproc.COLOR_RGB2GRAY);
+//        Imgproc.threshold(gray,dst_bin, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
+//        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+//        Imgproc.findContours(dst_bin,contours,hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE  );
+////        Imgproc.cvtColor(gray , gray, Imgproc.COLOR_GRAY2BGRA,4);
+////        Imgproc.drawContours(gray, contours, -1,  new Scalar(255, 0, 0), 1);
+//
+//
+////        for(int i = 0; i < contours.size(); i++){
+////                    Imgproc.polylines(image, contours, true, new Scalar(0, 255, 0), 2);
+////        }
+//
+//
+//        Log.d("tag_contours.size()", "" + contours.size());
+//
+//        ArrayList<MatOfPoint2f> approx = new ArrayList<MatOfPoint2f>();
+//        ArrayList<MatOfPoint> out = new ArrayList<MatOfPoint>();
+//
+//        //輪郭が四角形かどうかの判定
+//        for(int i = 0; i < contours.size(); i++) {
+//            MatOfPoint2f mop2f1 = new MatOfPoint2f();
+//            MatOfPoint2f mop2f2 =new MatOfPoint2f();
+//            MatOfPoint mop = new MatOfPoint();
+//            contours.get(i).convertTo(mop2f1, CvType.CV_32FC2);
+//
+//            //直線近似
+//            Imgproc.approxPolyDP(mop2f1,mop2f2, 10.0, true);
+//            mop2f2.convertTo(mop, CvType.CV_32FC2);
+//            out.add(mop);
+//            approx.add(mop2f2);
+//            double area =Imgproc.contourArea(approx.get(i));
+//            Log.d("tag_area", "" + area);
+//            Log.d("tag_approx.size()", "" + approx.size());
+//
+//            //近似が４点かつ面積が一定以上
+//            if(approx.size() == 4 && area > 200) {
+//                Imgproc.polylines(image, contours, true, new Scalar(0, 255, 0), 2);
+//
+////                try {
+////                    Thread.sleep(1000);
+////                }catch (InterruptedException e){}
+//
+////                approx.clear();
+//            }
+//
+//        }
+//        return image;
+//    }
+
+    //画像合成お試し
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat image = inputFrame.rgba();
-        Mat gray = new Mat(3, 3, CvType.CV_32FC2);
-        Mat dst_bin = new Mat(3, 3, CvType.CV_32FC2);
-        Mat hierarchy = new Mat(3, 3, CvType.CV_32FC2);
+        Mat mat1 = new Mat();
+        Mat mat2 = new Mat();
         Resources r = getResources();
-        Bitmap bmp_frame = BitmapFactory.decodeResource(r, R.drawable.image);
-//        bmp_frame = Bitmap.createScaledBitmap(bmp_frame, 640, 480, false);
-        Mat mat = new Mat(3,3,CvType.CV_32FC2);
-        Core.add(mat, image, image);
-        Imgproc.pyrDown(image, image);
-        Imgproc.pyrUp(image, image);
-        Imgproc.cvtColor(image , gray, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.threshold(gray,dst_bin, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
-        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Imgproc.findContours(dst_bin,contours,hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE  );
-//        Imgproc.cvtColor(gray , gray, Imgproc.COLOR_GRAY2BGRA,4);
-//        Imgproc.drawContours(gray, contours, -1,  new Scalar(255, 0, 0), 1);
+        Bitmap bmp_frame1 = BitmapFactory.decodeResource(r, R.drawable.image);
+        bmp_frame1 = Bitmap.createScaledBitmap(bmp_frame1, 1280, 720, false);
+        Bitmap bmp_frame2 = BitmapFactory.decodeResource(r, R.drawable.image);
+        bmp_frame2 = Bitmap.createScaledBitmap(bmp_frame2, 500, 500, false);
+        Utils.bitmapToMat(bmp_frame1, mat1);
+        Utils.bitmapToMat(bmp_frame2, mat2);
+
+        paste(mat1, mat2, 10, 10, 500, 500);
+
+        return mat1;
 
 
-//        for(int i = 0; i < contours.size(); i++){
-//                    Imgproc.polylines(image, contours, true, new Scalar(0, 255, 0), 2);
-//        }
+    }
 
+    public void paste(Mat dst, Mat src, int x, int y, int width, int height){
+        int w, h, u, v, px, py;
+        Mat resized_img = new Mat();
+        Size size = new Size(width, height);
+        Imgproc.resize(src, resized_img, size);
 
-        Log.d("tag_contours.size()", "" + contours.size());
-
-        ArrayList<MatOfPoint2f> approx = new ArrayList<MatOfPoint2f>();
-        ArrayList<MatOfPoint> out = new ArrayList<MatOfPoint>();
-
-        //輪郭が四角形かどうかの判定
-        for(int i = 0; i < contours.size(); i++) {
-            MatOfPoint2f mop2f1 = new MatOfPoint2f();
-            MatOfPoint2f mop2f2 =new MatOfPoint2f();
-            MatOfPoint mop = new MatOfPoint();
-            contours.get(i).convertTo(mop2f1, CvType.CV_32FC2);
-
-            //直線近似
-            Imgproc.approxPolyDP(mop2f1,mop2f2, 10.0, true);
-            mop2f2.convertTo(mop, CvType.CV_32FC2);
-            out.add(mop);
-            approx.add(mop2f2);
-            double area =Imgproc.contourArea(approx.get(i));
-            Log.d("tag_area", "" + area);
-            Log.d("tag_approx.size()", "" + approx.size());
-
-            //近似が４点かつ面積が一定以上
-            if(approx.size() == 4 && area > 200) {
-                Imgproc.polylines(image, contours, true, new Scalar(0, 255, 0), 2);
-
-//                try {
-//                    Thread.sleep(1000);
-//                }catch (InterruptedException e){}
-
-//                approx.clear();
+        if (x >= dst.cols() || y >= dst.rows()){}
+        else{
+            if(x >= 0){
+                w = Math.min(dst.cols() - x, resized_img.cols());
+                u = 0;
+            }else{
+                w = Math.min(Math.max(resized_img.cols() + x, 0), dst.cols());
+                u = Math.min(-x, resized_img.cols() - 1);
+            }
+            if(y >= 0){
+                h = Math.min(dst.rows() - y, resized_img.rows());
+                v = 0;
+            }else{
+                h = Math.min(Math.max(resized_img.rows() + y, 0), dst.rows());
+                v = Math.min(-y, resized_img.rows() - 1);
             }
 
+            px = Math.max(x, 0);
+            py = Math.max(y, 0);
+
+            Mat roi_dst = new Mat(dst, new Rect(px, py, w, h));
+            Mat roi_resized = new Mat(resized_img, new Rect(u, v, w, h));
+            roi_resized.copyTo(roi_dst);
         }
-        return image;
     }
 
 
-//    private Mat fncDrwLine(Mat lin,Mat img) {
-//        double[] data;
-//        Point pt1 = new Point();
-//        Point pt2 = new Point();
-//        for (int i = 0; i < lin.cols(); i++){
-//            data = lin.get(0, i);
-//            pt1.x = data[0];
-//            pt1.y = data[1];
-//            pt2.x = data[2];
-//            pt2.y = data[3];
-//            Log.d("log", "pt1.x : " + pt1.x + " pt1.y : " + pt1.y + "pt2.x : " + pt2.x + "pt2.y : " + pt2.y);
-//            Imgproc.line(img, pt1, pt2, new Scalar(255, 0, 0), 10);
-//        }
-//        return img;
-//    }
 
-//    public void DrawLine(Mat image){
-//        double[] data;
-//        double rho, theta;
-//        Point pt1 = new Point();
-//        Point pt2 = new Point();
-//        double x0, y0;
-//
-//        for (int i = 0; i == lines.cols(); i++){
-//            data = lines.get(0, i);
-//            rho = data[0];
-//            theta = data[1];
-//            double cosTheta = Math.cos(theta);
-//            double sinTheta = Math.sin(theta);
-//            pt1.x = rho / cosTheta;
-//            pt1.y = 0;
-//            pt2.x = 0;
-//            pt2.y = rho / sinTheta;
-//
-//            Core.line(image, pt1, pt2, new Scalar(255, 0, 0), 1);
-//        }
-//    }
 }
